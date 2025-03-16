32598fb0f37dd27ed9a90c4314d6c553b6f23389
xiBread <oscar.leevermeren@gmail.com>
2025-03-15 23:16:56 -0400

    refactor: sort profiles

--- src/routes/profiles/+page.server.ts
+++ src/routes/profiles/+page.server.ts
@@ -1,7 +1,9 @@
 import { db } from "$lib/server/db";
 
 export async function load() {
-	const profiles = await db.query.profile.findMany();
+	const profiles = await db.query.profile.findMany({
+		orderBy: (t, { asc, desc }) => [asc(t.firstName), desc(t.birthdate)],
+	});
 
 	return { profiles };
 }
